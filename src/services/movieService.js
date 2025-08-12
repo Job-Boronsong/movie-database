c59612e019629e86f2e3dcb6ddeb641dffc61b9e
Job Boronsong <jboronsong@gmail.com>
2025-08-12 07:14:47 +0000

    update movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,11 +1,18 @@
+// src/services/movieService.js
 import axios from "axios";
-
 const API_KEY = import.meta.env.VITE_APP_MOVIE_API_KEY;
 const BASE_URL = "https://www.omdbapi.com/";
 
-export const searchMovies = async (query) => {
+export const searchMovies = async (q, page = 1) => {
   const { data } = await axios.get(BASE_URL, {
-    params: { s: query, apikey: API_KEY },
+    params: { s: q, page, apikey: API_KEY }
+  });
+  return data; // OMDb returns Response: "True"/"False" and Search: [...]
+};
+
+export const getMovieDetails = async (imdbID) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { i: imdbID, plot: "full", apikey: API_KEY }
   });
   return data;
 };
