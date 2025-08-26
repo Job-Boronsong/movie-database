f30e0855d055b11ff4ba8b580671a5d2fd8a7db8
Job Boronsong <jboronsong@gmail.com>
2025-08-26 15:39:10 +0000

    Add Loading Spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+// src/components/Spinner.jsx
+export default function Spinner() {
+    return (
+      <div className="flex justify-center items-center mt-6">
+        <div className="w-8 h-8 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
